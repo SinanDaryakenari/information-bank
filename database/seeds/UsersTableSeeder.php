0c51d6d339d95a9023710830b1d2481bac4cc302
FurkanHaskol <furkanhaskol@hotmail.com>
2020-11-26 16:29:48 +0300

    Haskol

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Role;
-use App\Models\User;
+use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
